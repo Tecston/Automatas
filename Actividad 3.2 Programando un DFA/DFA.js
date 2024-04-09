3576171dd30135b5e2faeef8eb804b574c07b7fd
Tecston <jrmedver2004@gmail.com>
2024-04-09 04:26:53 -0700

    .

---
+++ Actividad 3.2 Programando un DFA/DFA.js
@@ -0,0 +1,15 @@
+// Se definen los tokens
+const TOKEN_TYPES = {
+    INTEGER: 'Entero',
+    FLOAT: 'Real',
+    ASSIGNMENT: 'Asignación',
+    ADDITION: 'Suma',
+    SUBTRACTION: 'Resta',
+    MULTIPLICATION: 'Multiplicación',
+    DIVISION: 'División',
+    POWER: 'Potencia',
+    VARIABLE: 'Variable',
+    LEFT_PARENTHESIS: 'Paréntesis que abre',
+    RIGHT_PARENTHESIS: 'Paréntesis que cierra',
+    COMMENT: 'Comentario'
+};
